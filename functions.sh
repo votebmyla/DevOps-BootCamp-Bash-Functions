2f4965ed9d2e73e2d661ebb121182b35d494aeff
votebmyla <votebmyla@gmail.com>
2022-12-22 01:22:11 +0500

    add shortest string function task solution

--- functions.sh
+++ functions.sh
@@ -3,3 +3,31 @@
 pow() {
   echo $(($1 ** $2))
 }
+
+shortest() {
+  # length of first argument
+  len=$(echo ${@} | cut -d' ' -f1)
+  result=()
+  # iterate arguments
+  for p in "${@}"; do
+    # check element length
+    if [ ${#p} -lt ${#len} ] || [ ${#p} -eq ${#len} ]; then
+      # check element length
+      if [ ${#p} -eq ${#len} ]; then
+        # add element to the array
+        result=(${result[@]} "${p}")
+        # start next iteration
+        continue
+      fi
+      # change minimum length
+      len=${p}
+      # add element to the array
+      result=("${p}")
+    fi
+
+  done
+  # output result element
+  for e in ${result[@]}; do
+    echo ${e}
+  done
+}
